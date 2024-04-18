f9c7124795c9f2d6de1129e9c2809239b7a9d29e
Benjamin Fogalele <benjamin.fogalele@ntnu.no>
2024-04-18 18:40:43 +0200

    Renders data

--- src/components/StoreViewComponents/ItemContent.vue
+++ src/components/StoreViewComponents/ItemContent.vue
@@ -1,9 +1,20 @@
 <template>
-  <LoadingIndicator v-if="true" />
+  <div class="content">
+    <h2>{{ item.title }}</h2>
+    <p>{{ item.description }}</p>
+    <button @click="$emit('addItem', item)">Add to cart</button>
+  </div>
 </template>
 
 <script setup lang="ts">
 import LoadingIndicator from '../../components/LoadingIndicator.vue'
+
+defineProps({
+  item: {
+    type: Object,
+    required: true
+  }
+})
 </script>
 
 <style scoped></style>
--- src/views/StoreView.vue
+++ src/views/StoreView.vue
@@ -1,14 +1,22 @@
 <template>
-  <LoadingIndicator v-if="true" />
+  <LoadingIndicator v-if="!data" />
+  <ItemOverview v-else :items="data" />
+
 </template>
 
 <script setup lang="ts">
 import { useFetchData } from '../utils/useFetchData'
 import LoadingIndicator from '../components/LoadingIndicator.vue'
+import ItemOverview from '../components/StoreViewComponents/ItemOverview.vue'
+import { ref } from 'vue';
+import type { StoreItem } from '../types';
 
 const { fetchData } = useFetchData()
 
+const data = ref<StoreItem[]>()
+
 fetchData().then((json) => {
   console.log(json)
+  data.value = json
 })
 </script>
